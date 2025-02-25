c7d0e3fe6d01dfbc8f8956bfee6864e73d880a4f
João Victor Carvalho Alves <canaldolagyt@gmail.com>
2025-02-25 16:20:25 -0300

    Update questões da aula 1.c

--- questões da aula 1.c
+++ questões da aula 1.c
@@ -1,31 +1,31 @@
 // questão 1
-// #include<stdio.h>
-// #include<locale.h>
-// #include <stdlib.h>
-// int main()
-// {
-//     setlocale(LC_ALL, "Portuguese");
-//     double litros = 3.4;
-//     double * ptr;
-//     ptr = &litros;
+#include<stdio.h>
+#include<locale.h>  
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    double litros = 3.4;
+    double * ptr;
+    ptr = &litros;
 
-//         printf("\nesse e o valor de litros %.1f", *ptr);
+        printf("\nesse e o valor de litros %.1f", *ptr);
 
-//     return 0;
-// }
+    return 0;
+}
 
-// questão 2
-// #include<stdio.h>
-// #include<locale.h>
-// int main()
-// {
-//     float torque[10]={2.5,8.1,3.4,9.2,5.7,9.6,6.3,8.0,5.4,4.9};
-//     float * ptr1;
-//     float * ptr2;
-//     ptr1 = &torque[0];
-//     ptr2 = &torque[9];
+//questão 2
+#include<stdio.h>
+#include<locale.h>
+int main()
+{
+    float torque[10]={2.5,8.1,3.4,9.2,5.7,9.6,6.3,8.0,5.4,4.9};
+    float * ptr1;
+    float * ptr2;
+    ptr1 = &torque[0];
+    ptr2 = &torque[9];
 
-//     printf("o primeiro e:%.1f\no segundo e:%.1f",*ptr1, *ptr2);
-//     return 0;
-// }
+    printf("o primeiro e:%.1f\no segundo e:%.1f",*ptr1, *ptr2);
+    return 0;
+}
+
 
